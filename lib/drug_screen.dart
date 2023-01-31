8ea8c541e578a1c2bbe7802e691a8470e233d4e7
DERRICK ZZIWA <derricktab44@gmail.com>
2023-01-31 20:57:52 +0300

    drug screen implemented

---
+++ lib/drug_screen.dart
@@ -0,0 +1,148 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+
+class DrugScreen extends StatefulWidget {
+  final String drugName;
+  final String description;
+  final String imagePath;
+  final String adultDosage;
+  final String childDosage;
+  const DrugScreen(
+      {super.key,
+      required this.drugName,
+      required this.description,
+      required this.imagePath,
+      required this.adultDosage,
+      required this.childDosage});
+
+  @override
+  State<DrugScreen> createState() => _DrugScreenState();
+}
+
+class _DrugScreenState extends State<DrugScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text("${widget.drugName} Dosage"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          // DRUG IMAGE
+          Image.asset(
+            widget.imagePath,
+            height: 320,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 30),
+
+          // DRUG NAME
+          Text(
+            widget.drugName,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: Colors.orange,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          // BRIEF INFO ABOUT THE DRUG
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Text(
+              widget.description,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 40),
+
+          // ADULT DOSAGE
+          Container(
+            margin: const EdgeInsets.only(right: 240, left: 10),
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
+            ),
+            width: MediaQuery.of(context).size.width * 0.1,
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Text(
+              "ADULT DOSE",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // DOSAGE TEXT
+          DottedBorder(
+            borderPadding:
+                const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            radius: const Radius.circular(20),
+            borderType: BorderType.RRect,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30,
+              vertical: 20,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(widget.adultDosage),
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          // PEDIATRIC DOSAGE
+          Container(
+            margin: const EdgeInsets.only(right: 240, left: 10),
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
+            ),
+            width: MediaQuery.of(context).size.width * 0.1,
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Text(
+              "CHILD DOSE",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // CHILD DOSAGE TEXT
+          DottedBorder(
+            borderPadding:
+                const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            radius: const Radius.circular(20),
+            borderType: BorderType.RRect,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30,
+              vertical: 25,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(widget.childDosage),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/drugs_and_dosages.dart
+++ lib/drugs_and_dosages.dart
@@ -1,4 +1,6 @@
+import 'package:amr_app/drug_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:outline_search_bar/outline_search_bar.dart';
 
 class DrugsAndDosages extends StatefulWidget {
   const DrugsAndDosages({super.key});
@@ -12,12 +14,64 @@ class _DrugsAndDosagesState extends State<DrugsAndDosages> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dosage Of Antibiotics Drugs"),
+        title: const Text("Drugs and Dosages"),
       ),
       body: ListView(
         children: [
-          SizedBox(height: 20,)
-          
+          const SizedBox(
+            height: 20,
+          ),
+
+          // SEARCH BAR
+          OutlineSearchBar(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 30,
+              vertical: 20,
+            ),
+            borderColor: Colors.grey,
+            borderRadius: BorderRadius.circular(40),
+            hintText: 'Search Drug',
+            elevation: 5,
+          ),
+
+          // LIST OF DRUGS
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return const DrugScreen(
+                          drugName: "Amoxicillin",
+                          description:
+                              "Amoxicillin is a penicillin antibiotic. It is used to treat bacterial infections, such as chest infections (including pneumonia) and dental abscesses.",
+                          adultDosage: "500 - 1000mg Thrice a day (PO or IV)",
+                          childDosage:
+                              '15-20 mg / kg twice daily oral \n 30-35 mg /kg thrice daily IV',
+                          imagePath: "assets/drug_images/amoxillin.jpg",
+                        );
+                      },
+                    ),
+                  );
+                },
+                child: Card(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  elevation: 5,
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 17, horizontal: 20),
+                    child: const Text("Amoxicillin"),
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
